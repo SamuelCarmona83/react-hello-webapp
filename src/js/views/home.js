1352c75ac702922aa4d6f172e6c644138c67e782
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2024-02-17 02:33:20 +0000

    :memo: We have an infinite loop ausilio

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -21,21 +21,22 @@ function generateCard(){
 const getScore = cardList => {
 	let points = 0;
 
+	const addSimbols = card => {
+		if(numbers.includes(card.value)){
+			points += card.value
+		}
+
+		if(['Q', 'K', 'J'].includes(card.value)){
+			points += 10
+		}
+	}
+
 	//buscar si existen A's 
-	const aCardIsA = cardList.find( card => card.value == 'A') // find
-	
+	const aCardIsA = cardList.find( card => card.value == 'A')
+
 	if(aCardIsA){
 
-		cardList.filter( x => x.value != 'A').forEach( card => {
-			
-			if(numbers.includes(card.value)){
-				points += card.value
-			}
-	
-			if(['Q', 'K', 'J'].includes(card.value)){
-				points += 10
-			}
-		});
+		cardList.filter( x => x.value != 'A').forEach(addSimbols);
 
 		cardList.filter( x => x.value == 'A').forEach( ases => {
 			if( points <= 10 ){
@@ -47,56 +48,118 @@ const getScore = cardList => {
 
 
 	}else{
-		cardList.forEach( card => {
-				
-			if(numbers.includes(card.value)){
-				points += card.value
-			}
-	
-			if(['Q', 'K', 'J'].includes(card.value)){
-				points += 10
-			}
-		});
+		cardList.forEach(addSimbols);
 	}
 	return points
 }
 
 export const Home = () => {
 
-	const [ playerCards, setPlayerCards ] = useState([
-		{
-			"pinta": "♣",
-			"value": "A",
-			"color": "black"
-		},
-		{
-		  "pinta": "♠",
-		  "value": 9,
-		  "color": "black"
-		},
-	  ])
+	const [ playerCards, setPlayerCards ] = useState([])
+	const [ croupierCards, setCroupierCards ] = useState([])
 
-	// useEffect(()=>{
-	// 	const firstCard = generateCard()
-	// 	const secondCard = generateCard()
-	// 	const playerFirstHand = [ firstCard , secondCard ]
-	// 	setPlayerCards(playerFirstHand)
+	const [ stay, setStay ] = useState(false)
 
-	// 	// Verificar si esto es Black Jack 21
+	useEffect(()=>{
+		const firstCard = generateCard()
+		const secondCard = generateCard()
+		const playerFirstHand = [ firstCard , secondCard ]
+		setPlayerCards(playerFirstHand)
+
+		const croupierCards = Array(2).fill(generateCard())
+		setCroupierCards(croupierCards)
+		// Verificar si esto es Black Jack 21
 		
-	// },[])
+	},[]) // onLoad
+
+	useEffect(()=>{
+		if(getScore(playerCards) >= 21 && getScore(croupierCards) <= 21){
+			setStay(true)
+		}
+	},[playerCards])
+
+	useEffect(()=>{
+		if(getScore(croupierCards) <= 17 && stay){
+			setCroupierCards([ ...croupierCards, generateCard() ])
+		}
+	}, [stay, croupierCards]) // update
 
 	const addCardToHand = () => {
 		setPlayerCards([ ...playerCards, generateCard() ])
 	}
 
-	
+	const whoIsWining = () => {
+
+		const playerPoints = getScore(playerCards)
+		const croupierPoints = getScore(croupierCards)
+
+		if(playerPoints > 21){
+			return 'Croupier Wins'
+		}
+
+		if( croupierPoints == 21){
+			setStay(true)
+			return 'Croupier Wins'
+		}
+
+		if( playerPoints == 21){
+			setStay(true)
+			return 'Player Wins!'
+		}
+
+		if(playerPoints == croupierPoints){
+			return 'Everyone Wins'
+		}
+
+		if( stay && playerPoints <= 21 && playerPoints > croupierPoints){
+			return 'Player Wins!'
+		}
+
+		if( stay && croupierPoints > 21 && playerPoints <= 21){
+			return 'Player Wins!'
+		}
+
+		return 'Croupier Wins'
+	}
 
 	return <div className="text-center h-100 d-flex flex-column justify-content-center">
-		<h1>Player Hand! { getScore(playerCards) }</h1>
+
+		{ stay && <h1 className="text-white">{whoIsWining()}</h1>}
+		
+		<h1 style={{ color: 'white' }}>Croupier Hand { getScore(croupierCards) }</h1>
+
+		{
+			!stay &&  croupierCards[0] && <div className="d-flex flex-row justify-content-center">
+				<Card key={'croupier-card-1'} content={croupierCards[0] } />
+				<div className="card p-2" style={{ width: '320px', 
+					margin: '10px',
+					height: '200px',
+					fontSize: '24px',
+				}}>
+					<div className="d-flex">
+					</div>
+					<div className="card-body d-flex">
+						<span className="mx-auto my-auto" style={{ fontSize: '36px'}}>
+							?
+						</span>
+					</div>
+					<div className="d-flex">
+					</div>
+				</div>
+			</div>
+		}
+
+		{stay && <div className="d-flex flex-row justify-content-center">
+			{ croupierCards.map( (pCard, ind) => <Card key={ind} content={pCard} />) }
+		</div>}
+
+		<h1 style={{ color: 'white' }}>Player Hand { getScore(playerCards) }</h1>
 		<div className="d-flex flex-row justify-content-center">
 			{ playerCards.map( (pCard, ind) => <Card key={ind} content={pCard} />) }
 		</div>
-		<button onClick={() => addCardToHand()} className="btn btn-warning ">Ask</button>
+		{(getScore(playerCards) < 21 && !stay) &&  <div>
+			<button onClick={() => addCardToHand()} className="btn btn-warning ">Ask</button>
+		</div>}
+		<button onClick={() => stay ? () => {} : setStay(true)} className="btn btn-success ">Stay</button>
 	</div>
 };
